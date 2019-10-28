609f504ca8306caec1dcc139c128f086148b34c9
smooth97 <dbtmdwls0506@naver.com>
2019-10-28 12:29:32 +0900

    html

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-class Food {
+class Foods {
   constructor(name) {
     this.name = name;
-    this.brans = [];
+    this.brands = [];
   }
 
-  // 메소드 생성 (prototpye)
   addBrand(brand) {
-    this.brans.push(brand);
+    this.brands.push(brand);
   }
 
   print() {
-    console.log(`${this.name}을/를 파는 음식점들`);
-    console.log(this.brans.join(", "));
+    console.log(`${this.name}을 파는 음식점`);
+    console.log(this.brands.join(", "));
   }
 }
 
-const pizza = new Food("피자");
-pizza.addBrand("피자헛");
-pizza.addBrand("도미노 피자");
-
-const chicken = new Food("치킨");
-chicken.addBrand("굽네치킨");
-chicken.addBrand("페리카나");
-
-pizza.print();
-// 피자을/를 파는 음식점들
-// 피자헛, 도미노 피자
-chicken.print();
-// 치킨을/를 파는 음식점들
-// 굽네치킨, 페리카나
+const taco = new Foods("타코");
+taco.addBrand("타코벨");
+taco.print();
